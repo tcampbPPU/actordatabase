e1a00ccf94673bb917d0759e6e54f25013c63883
Tanner Campbell <tcamp022@gmail.com>
2017-10-26 17:56:25 -0400

    Week 8

--- dbt-connection.js
+++ dbt-connection.js
@@ -10,12 +10,12 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT *  FROM users;", function(err, rows, fields) {
-//    con.query("INSERT INTO users(first_name, last_name, is_admin, email) VALUES (?, ?, ?, ?);",  ["Tanner", "Campbell", 0, "tcampb@pointpark.edu"], function(err, rows, fields) {
-    if (err) throw err;
-    console.log("Connected!");
-//  console.log(fields);
-    console.log(rows);
-    con.end();
-  });
+//  con.query("SELECT *  FROM users;", function(err, rows, fields) {
+//    con.query("INSERT INTO users(first_name, last_name, usr, email, home_phone, cell_phone, address, is_admin,) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",  ["", "", "", "", "", "", "", 0], function(err, rows, fields) {
+      if (err) throw err;
+      console.log("Connected!");
+//      console.log(rows);
+      con.end();
+//    });
+//  });
 });
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 //var credentials = require('./credentials.js');
-// var expressValidator = require('express-validator');
-// var formidable = require('formidable');
+var expressValidator = require('express-validator');
+var formidable = require('formidable');
 var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host : 'fkonat.it.pointpark.edu',
+  user : 'lunamista',
+  password : 'lunamista123',
+  database : 'lunadb'
+});
+
 var fs = require('fs');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:"main"});
@@ -15,6 +23,9 @@ app.use( function( req, res, next){
   res.locals.showTests = app.get(' env') !== 'production' && req.query.test === '1';
   next();
  });
+
+var bodyParser = require('body-parser');
+
 // Skip till Node-modules get updated
 /*
 app.use(require('body-parser').urlencoded({extended:true}));
@@ -26,22 +37,55 @@ app.use(require('express-session')({
 }));
 */
 
+// Call to Express body parser json for DB querys and inserts
+app.use(bodyParser.json());
+
 app.get("/", function(req,res){
     res.render("home");
 });
 
-// Redirect to new page after sign on
-
 app.post('/', [function(req, res, next) {
   next();
 }, function(req, res) {
-  res.render('addUser');
+  res.send('Hello World!');
 }]);
+
+/* TO CHECK user login with DB records
+app.post("/", function (req, res) {
+//  console.log(req.body.user.email);
+//  console.log(req.body.user.pwd);
+  var email = req.body.user.email;
+  var pwd = req.body.user.pwd;
+  var q = "SELECT * FROM users WHERE user[email] LIKE '%" + email +"%'" AND user[pwd] LIKE '%" + pwd +"%'";
+  con.query(q, function(err, results) {
+    if (err) throw err;
+      res.send({success: results});
+});
+*/
 
 app.get("/addUser", function(req,res){
     res.render("addUser");
 });
 
+// Gets From data from addUser Page, then redirects
+app.post('/addUser', [function(req, res, next){
+//  console.log(request.body.user.first_name);
+//  console.log(request.body.user.last_name);
+//  console.log(request.body.user.usr_name);
+//  console.log(request.body.user.email);
+//  console.log(request.body.user.home_phone);
+//  console.log(request.body.user.cell_phone);
+//  console.log(request.body.user.address);
+var sql = "INSERT INTO Test (first_name, last_name, usr_name, email, home_phone, cell_phone, address, is_admin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+var values = [
+    [request.body.user.first_name, request.body.user.last_name, request.body.user.usr_name, request.body.user.email, request.body.user.home_phone, request.body.user.cell_phone, request.body.user.address, 0],
+];
+con.query(sql, [values], function(err) {
+    if (err) throw err;
+    con.end();
+  //next();
+  }, 
+}]);
 
 
 
@@ -97,7 +141,8 @@ app.get("/forgotpassword", function(req,res){
 app.get("/search", function(req,res){
   res.render("search");
 });
-*/
+END*/
+
 //custom 404 page
 app.use(function(req, res){
   res.status(404);
