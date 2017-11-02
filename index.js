1bcc02e976da8c5b005bf57936190990be6a6bf2
adschaf <adschaf@pointpark.edu>
2017-11-02 17:53:15 -0400

    Indentation and Grammar Fixes

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ app.engine("handlebars",handlebars.engine);
 app.set("view engine","handlebars");
 app.set('port', process.env.PORT || 4000);
 app.use(express.static(__dirname +'/public'));
+
 app.use( function( req, res, next){
   res.locals.showTests = app.get(' env') !== 'production' && req.query.test === '1';
   next();
  });
+
 app.use(require('body-parser').urlencoded({extended:true}));
 //app.use(require('cookie-parser')(credentials.cookieSecret));
 //app.use(require('express-session')({
@@ -29,40 +31,44 @@ app.use(require('body-parser').urlencoded({extended:true}));
  //saveUninitialized:false,
 // secret:credentials.cookieSecret
 //}));
+
 //app.get("/", function(req,res){
 //    res.render("home");
 //});
+
 // Root Dir
 app.get('/', function(req, res) {
   res.render('landing', {
     menu: [{"page": "home", "label": "Home"}, {"page": "about", "label": "About"}]
   });
 });
+
 app.get('/admin_page', function(req, res) {
         res.render('admin_page');
 });
 
 app.get('/admin_mail', function(req, res) {
         res.render('admin_mail');
-})
+});
+
 app.get('/admin_search', function(req, res) {
         res.render('admin_search');
 });
 
 app.post('/process-search', function(req, res) {
-         var search = req.body.search;
-       // console.log(search);
-        var q = "SELECT * FROM users WHERE first_name LIKE '%" + search +"%'";
-        connection.query(q, function(err, results) {
-         if (err) throw err;
-           res.send({success: results});
-         });
+  var search = req.body.search;
+  // console.log(search);
+  var q = "SELECT * FROM users WHERE first_name LIKE '%" + search +"%'";
+  connection.query(q, function(err, results) {
+  if (err) throw err;
+  res.send({success: results});
+  });
 });
 
 
 app.get("/history", function(req,res){
   if(req.session.admin_id){
-  }else {
+  } else {
     res.render("searchhistory",{admin:req.session.firstName,adminlogin:req.session.admin_id});
   }
 });
@@ -70,9 +76,11 @@ app.get("/history", function(req,res){
 app.get("/creatnewaccount", function(req,res){
   res.render("addUser");
 });
+
 app.get("/forgotpassword", function(req,res){
   res.render("forgotpassword");
 });
+
 app.get("/search", function(req,res){
   res.render("search");
 });
@@ -82,6 +90,7 @@ app.use(function(req, res){
   res.status(404);
   res.render("404");
 });
+
 //custom 500 page
 app.use(function(err, req, res, next){
   console.log(err.stack);
@@ -90,5 +99,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-console.log('listening on http:// localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log("listening on http:// localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
 });
