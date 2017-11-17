cb309d120486b85748bf36360c3bb24ecc54868b
Matt Alexander <bigmatal87@hotmail.com>
2017-11-17 13:50:39 -0500

    added forgot & resetpassword

--- index.js
+++ index.js
@@ -48,6 +48,17 @@ function connect(cb){
   });
 }
 
+//app.use(express.static(__dirname));
+//app.use(require('sesame')()); // for sessions
+
+var forgot = require('../../')({
+    uri : '/resetpassword',
+    from : 'mdalexa@pointpark.edu',
+    host : '/forgot', port : 4000,
+});
+
+//app.use(forgot.middleware);
+
 function getMenu(req){
   var menu =[];
   var isAdmin = req.session.is_admin;
@@ -232,6 +243,32 @@ app.post('/delete-in-database', function(req, res){
    }
 });
 
+app.post('/forgot', function (req, res) {
+  var email = req.body.email;
+  var reset = forgot(email, function (err) {
+      if (err) res.end('Error sending message: ' + err)
+      else res.end('Check your inbox for a password reset message.')
+  });
+    
+  reset.on('request', function (req_, res_) {
+      req_.session.reset = { email : email, id : reset.id };
+      fs.createReadStream(__dirname + '/forgot.html').pipe(res_);
+  });
+});
+
+app.post('/resetpassword', function (req, res) {
+  if (!req.session.reset) return res.end('reset token not set');
+    
+  var password = req.body.password;
+  var confirm = req.body.confirm;
+  if (password !== confirm) return res.end('passwords do not match');
+    
+  // update the user db here    
+
+  forgot.expire(req.session.reset.id);
+  delete req.session.reset;
+  res.end('password reset');
+});
 
 app.post('/process-search', function(req, res) {
   var search = req.body.search;
